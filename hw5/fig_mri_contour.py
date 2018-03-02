a08a33336149a1aa25e10e13e4cf439e22917f20
patrickbeekman <patrickbeekman1095@gmail.com>
2018-03-02 14:32:48 -0500

    changed to use imshow instead of scatterplot. also changed ordering of bit creation pattern.

--- hw5/fig_mri_contour.py
+++ hw5/fig_mri_contour.py
@@ -7,6 +7,8 @@ import pandas as pd
 
 threshold = 40
 lines = []
+points = []
+values = []
 fig = plt.figure()
 ax = fig.add_subplot(111)
 
@@ -22,7 +24,7 @@ def main():
         max = int(str(file.read(2))[2:4])
         file.read(1)
         #image = np.fromfile(file, dtype=np.uint16).reshape((height, width))
-
+        global image
         image = np.zeros((width, height))
         for i in range(0, width):
             whole_line = str(file.readline())[2:-4].split(" ")
@@ -38,19 +40,22 @@ def main():
     i = i.reshape((-1,))
     j = j.reshape((-1,))
     c = image.reshape((-1,))
-    ax.scatter(j, i, c=c, cmap='gray')
+    #ct = thresholded.reshape((-1,))
+    #ax.scatter(j, i, c=c, cmap='gray')
+    ax.imshow(image, cmap='gray')
 
     for n in range(width-1):
         for m in range(height-1):
-            bit = bit_pattern(thresholded[n,m], thresholded[n+1,m], thresholded[n+1, m-1], thresholded[n, m-1])
-            # create bit pattern
-            # pass into lookup with n and m
+            #bit = bit_pattern(thresholded[n,m], thresholded[n+1,m], thresholded[n+1, m-1], thresholded[n, m-1])
+            bit = bit_pattern(thresholded[n,m-1], thresholded[n+1,m-1], thresholded[n+1, m], thresholded[n, m])
             lookup(bit, m, n)
-            #print(m,",",n,"bit:",bit)
-
-    plt.show()
 
 
+
+    #plt.show()
+    plt.savefig('contour_14.png')
+
+# bottom left is start, top left is end
 def bit_pattern(first, second, third, fourth):
     bits = 0b0
     bits = (bits << 1) | int(first)
@@ -62,35 +67,133 @@ def bit_pattern(first, second, third, fourth):
 
 def lookup(val, i, j):
     if val == 1:
-        ax.add_line(Line2D([i, i+.5], [j-.5, j-1], color='b'))
+        points.append([i, j-.5])
+        values.append(image[j,i])
+        points.append([i+.5, j-1])
+        values.append(image[j-1,i])
+        curr = Line2D([i, i+.5], [j-.5, j-1], color='b')
+        lines.append(curr)
+        ax.add_line(curr)
     elif val == 2:
-        ax.add_line(Line2D([i+.5, i+1], [j-1, j-.5], color='b'))
+        points.append([i+.5, j-1])
+        values.append(image[j-1,i])
+        points.append([i+1, j-.5])
+        values.append(image[j,i+1])
+        curr = Line2D([i+.5, i+1], [j-1, j-.5], color='b')
+        lines.append(curr)
+        ax.add_line(curr)
     elif val == 3:
-        ax.add_line(Line2D([i, i+1], [j-.5, j-.5], color='b'))
+        points.append([i, j-.5])
+        values.append(image[j,i])
+        points.append([i+1, j-.5])
+        values.append(image[j,i+1])
+        curr = Line2D([i, i+1], [j-.5, j-.5], color='b')
+        lines.append(curr)
+        ax.add_line(curr)
     elif val == 4:
-        ax.add_line(Line2D([i+.5, i+1], [j, j-.5], color='b'))
+        points.append([i+.5, j])
+        values.append(image[j,i])
+        points.append([i+1, j-.5])
+        values.append(image[j,i+1])
+        curr = Line2D([i+.5, i+1], [j, j-.5], color='b')
+        lines.append(curr)
+        ax.add_line(curr)
     elif val == 5:
-        ax.add_line(Line2D([i, i+.5], [j-.5, j], color='b'))
-        ax.add_line(Line2D([i+.5, i+1], [j-1, j-.5], color='b'))
+        points.append([i, j-.5])
+        values.append(image[j,i])
+        points.append([i+.5, j])
+        values.append(image[j,i])
+
+        points.append([i+.5, j-1])
+        values.append(image[j-1,i])
+        points.append([i+1, j-.5])
+        values.append(image[j,i+1])
+        curr = Line2D([i, i+.5], [j-.5, j], color='b')
+        curr2 = Line2D([i+.5, i+1], [j-1, j-.5], color='b')
+        lines.append(curr)
+        lines.append(curr2)
+        ax.add_line(curr)
+        ax.add_line(curr2)
     elif val == 6:
-        ax.add_line(Line2D([i+.5, i+.5], [j, j-1], color='b'))
+        points.append([i+.5, j])
+        values.append(image[j,i])
+        points.append([i+.5, j-1])
+        values.append(image[j-1,i])
+        curr = Line2D([i+.5, i+.5], [j, j-1], color='b')
+        lines.append(curr)
+        ax.add_line(curr)
     elif val == 7:
-        ax.add_line(Line2D([i, i+.5], [j-.5, j], color='b'))
+        points.append([i, j-.5])
+        values.append(image[j,i])
+        points.append([i+.5, j])
+        values.append(image[j,i])
+        curr = Line2D([i, i+.5], [j-.5, j], color='b')
+        lines.append(curr)
+        ax.add_line(curr)
     elif val == 8:
-        ax.add_line(Line2D([i, i+.5], [j-.5, j], color='b'))
+        points.append([i, j-.5])
+        values.append(image[j,i])
+        points.append([i+.5, j])
+        values.append(image[j,i])
+        curr = Line2D([i, i+.5], [j-.5, j], color='b')
+        lines.append(curr)
+        ax.add_line(curr)
     elif val == 9:
-        ax.add_line(Line2D([i+.5, i+.5], [j, j-1], color='b'))
+        points.append([i+.5, j])
+        values.append(image[j,i])
+        points.append([i+.5, j-1])
+        values.append(image[j-1,i])
+        curr = Line2D([i+.5, i+.5], [j, j-1], color='b')
+        lines.append(curr)
+        ax.add_line(curr)
     elif val == 10:
-        ax.add_line(Line2D([i, i+.5], [j-.5, j-1], color='b'))
-        ax.add_line(Line2D([i+.5, i+1], [j, j-.5], color='b'))
+        points.append([i, j-.5])
+        values.append(image[j,i])
+        points.append([i+.5, j-1])
+        values.append(image[j-1,i])
+
+        points.append([i+.5, j])
+        values.append(image[j,i])
+        points.append([i+1, j-.5])
+        values.append(image[j,i+1])
+        curr = Line2D([i, i+.5], [j-.5, j-1], color='b')
+        curr2 = Line2D([i+.5, i+1], [j, j-.5], color='b')
+        lines.append(curr)
+        lines.append(curr2)
+        ax.add_line(curr)
+        ax.add_line(curr2)
     elif val == 11:
-        ax.add_line(Line2D([i+.5, i+1], [j, j-.5], color='b'))
+        points.append([i+.5, j])
+        values.append(image[j,i])
+        points.append([i+1, j-.5])
+        values.append(image[j,i+1])
+        curr = Line2D([i+.5, i+1], [j, j-.5], color='b')
+        lines.append(curr)
+        ax.add_line(curr)
     elif val == 12:
-        ax.add_line(Line2D([i, i+1], [j-.5, j-.5], color='b'))
+        points.append([i, j-.5])
+        values.append(image[j,i])
+        points.append([i+1, j-.5])
+        values.append(image[j,i+1])
+        curr = Line2D([i, i+1], [j-.5, j-.5], color='b')
+        lines.append(curr)
+        ax.add_line(curr)
     elif val == 13:
-        ax.add_line(Line2D([i+.5, i+1], [j-1, j-.5], color='b'))
+        points.append([i+.5, j-1])
+        values.append(image[j-1,i])
+        points.append([i+1, j-.5])
+        values.append(image[j,i+1])
+        curr = Line2D([i+.5, i+1], [j-1, j-.5], color='b')
+        lines.append(curr)
+        ax.add_line(curr)
     elif val == 14:
-        ax.add_line(Line2D([i, i+.5], [j-.5, j-1], color='b'))
+        points.append([i, j-.5])
+        values.append(image[j,i])
+        points.append([i+.5, j-1])
+        values.append(image[j-1,i])
+        curr = Line2D([i, i+.5], [j-.5, j-1], color='b')
+        lines.append(curr)
+        ax.add_line(curr)
 
 
 if __name__ == "__main__":
